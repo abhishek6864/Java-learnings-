907c16b16c14e07ddb4975301ff1460a730cb608
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 18:11:09 +0530

    Update exercise_8_3.java

--- exercise/ch_8/exercise_8_3.java
+++ exercise/ch_8/exercise_8_3.java
@@ -1,1 +1,25 @@
+class Square{
+  
+   float side;
+  
+    public void setSide(float s){
+      side = s;
+    }
+    
+  public void perimeter(){
+    System.out.printf("perimeter of square is %f \n", 4*side);
+  }
 
+  public void area(){
+    System.out.printf("area of square is %f",side*side);
+  }
+};
+
+class Main{
+ public static void main(String[] args){
+   Square s = new Square();
+   s.setSide(5);
+   s.perimeter();
+   s.area();
+  }
+}
