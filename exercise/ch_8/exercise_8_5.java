e254bde9ff9cc139aaa42a4e65eaaae2e6982371
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 18:21:47 +0530

    Create exercise_8_5.java

---
+++ exercise/ch_8/exercise_8_5.java
@@ -0,0 +1,25 @@
+class tommyVecetti{
+
+  public void hit(){
+    System.out.println("player is hitting bullets on enemy");
+  }
+
+  public void run(){
+    System.out.println("player is running away from enemy");
+  }
+
+  public void fire(){
+    System.out.println("player is firing gun");
+  }
+  
+};
+
+class Main{
+ public static void main(String[] args){
+   tommyVecetti player1 = new tommyVecetti();
+   player1.hit();
+   player1.run();
+   player1.fire();
+   
+  }
+}
