f5fb8a74e4d94c80a042bf465c888df0512fe5c2
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 17:14:36 +0530

    Create exercise_7_1.java

---
+++ exercise/ch_7/exercise_7_1.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void table(int n){
+    for(int i = 1; i<= 10; i++){
+      System.out.println(i*n);
+    }
+  }
+  public static void main(String[] args) {
+    table (7);
+  }
+}
