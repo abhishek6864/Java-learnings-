7b24ddb110d835684d32d8f6897992d3bb31023b
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 17:02:14 +0530

    Delete practise_6_5.java

--- exercise/practise_6_5.java
+++
@@ -1,8 +0,0 @@
-public class practise_6_5 {
-    public static void main(String[] args){
-        int [] arr = {3,7,62,27,1,93};
-        for(int i = arr.length-1; i >= 0;i--){
-            System.out.println(arr[i]);
-        }
-    }
-}
