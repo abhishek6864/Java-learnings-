47b7b20954dab8456a7d2522c2672c4a5759b872
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 16:00:16 +0530

    Create practise_3_2.java

---
+++ exercise/ch_3/practise_3_2.java
@@ -0,0 +1,13 @@
+class practise_3_1{
+    public static void main(String[] args){
+        String sentence = "hello world";
+        String upper = sentence.toUpperCase();
+        String lower = sentence.toLowerCase();
+        System.out.println(upper);
+        System.out.println(lower);
+
+
+
+
+    }
+}
