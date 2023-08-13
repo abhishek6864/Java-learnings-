6ac2f5f00186c691554a2e49ecb44f82a5ec68b1
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 16:01:07 +0530

    Create practise_3_3.java

---
+++ exercise/ch_3/practise_3_3.java
@@ -0,0 +1,10 @@
+class practise_3_3{
+    public static void main(String[] args){
+        String letter = "Dear <|name|>, thanks a lot";
+        letter = letter.replace("<|name|>","Abhishek");
+        System.out.println(letter);
+
+
+
+    }
+}
