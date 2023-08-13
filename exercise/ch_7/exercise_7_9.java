6d68b60f8afa1a32e26102400c9f420da6a3f88f
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-13 17:25:33 +0530

    Create exercise_7_9.java

---
+++ exercise/ch_7/exercise_7_9.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+
+  public static double c_f(double cel){
+    
+    return (cel*1.8+32.000);
+  }
+  
+  
+  public static void main(String[] args){
+    System.out.println(c_f(34.2));
+  }
+}
