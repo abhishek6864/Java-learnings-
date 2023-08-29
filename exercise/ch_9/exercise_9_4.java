a9522a8b38b629d055c660806e1a74ba308ac26a
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-29 09:48:30 +0530

    Create exercise_9_4.java

---
+++ exercise/ch_9/exercise_9_4.java
@@ -0,0 +1,35 @@
+class Rectangle{
+  double b;
+  double l;
+
+  public Rectangle(){
+    b = 5;
+    l = 4;
+  }
+
+  public Rectangle(float x, float y){
+    l = x;
+    b = y;
+  }
+
+  public void area(){
+    System.out.println("Area of rectangle is");
+    System.out.println(l*b);
+  }
+
+  public void perimeter(){
+    System.out.println("Perimeter of rectangle is");
+    System.out.println(2*(l+b));
+  }
+  
+};
+
+
+class Main {
+  public static void main(String[] args) {
+    Rectangle r1 = new Rectangle();
+    
+    r1.area();
+    r1.perimeter();
+  }
+}
