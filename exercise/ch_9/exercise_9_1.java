b803602a8e9f34f35fcd2294501ad6a52face693
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-29 09:34:51 +0530

    Create exercise_9_1.java

---
+++ exercise/ch_9/exercise_9_1.java
@@ -0,0 +1,24 @@
+class Cylinder{
+  double r;
+  double h;
+
+  public void setter(double a, double b){
+    r = a;
+    h = b;
+  }
+
+  public void area(){
+    System.out.println("Area of cylinder is ");
+    System.out.println(2*3.14*r*h);
+  }
+  
+};
+
+
+class Main {
+  public static void main(String[] args) {
+    Cylinder c1 = new Cylinder();
+    c1.setter(4,8.2);
+    c1.area();
+  }
+}
