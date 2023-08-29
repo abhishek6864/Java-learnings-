e50a4d409402164f334e4fc61f8aa5e5ba1a384e
Abhishek <103208789+abhishek0789@users.noreply.github.com>
2023-08-29 09:54:30 +0530

    Create exercise_9_5.java

---
+++ exercise/ch_9/exercise_9_5.java
@@ -0,0 +1,28 @@
+class Sphere{
+  double r;
+
+  public Sphere(double a){
+    r = a;
+  }
+
+  public void area(){
+    System.out.println("Area of sphere is ");
+    System.out.println(4*3.14*r*r);
+  }
+
+  public void volume(){
+    System.out.println("volume of sphere is ");
+    System.out.println((4/3)*3.14*r*r*r);
+  }
+
+  
+};
+
+
+class Main {
+  public static void main(String[] args) {
+    Sphere s1 = new Sphere(4.32);
+    s1.area();
+    s1.volume();
+  }
+}
